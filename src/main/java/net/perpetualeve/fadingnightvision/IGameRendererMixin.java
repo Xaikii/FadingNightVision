e841dd1e34f360a9cec19d680a0a2f2b2dc21ffd
Xaikii <xaikii@outlook.de>
2023-01-22 13:29:21 +0100

    1.19 rework

--- src/main/java/net/perpetualeve/fadingnightvision/IGameRendererMixin.java
+++
@@ -1,8 +0,0 @@
-package net.perpetualeve.fadingnightvision;
-
-public interface IGameRendererMixin {
-	
-	float overwriteTime(float num);
-	int overwriteLayer(int num);
-
-}
--- src/main/java/net/perpetualeve/fadingnightvision/mixins/GameRendererMixin.java
+++ src/main/java/net/perpetualeve/fadingnightvision/mixins/GameRendererMixin.java
@@ -7,39 +7,14 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
 
 import net.minecraft.client.renderer.GameRenderer;
-import net.minecraft.util.Mth;
-import net.minecraft.world.effect.MobEffects;
 import net.minecraft.world.entity.LivingEntity;
-import net.perpetualeve.fadingnightvision.IGameRendererMixin;
+import net.perpetualeve.fadingnightvision.FadingNightVision;
 
 @Mixin(GameRenderer.class)
-public class GameRendererMixin implements IGameRendererMixin {
-	
-	private static float i = 0.0f;
-	private static int l;
-	private static float t;
+public class GameRendererMixin {
 	
 	@Inject(method = "getNightVisionScale", at = @At("Invoke"), cancellable = true, locals = LocalCapture.CAPTURE_FAILEXCEPTION)
 	private static void getNightVisionScaleRed(LivingEntity p_109109_, float p_109110_, CallbackInfoReturnable<Float> ci) {
-		float time = p_109109_.getEffect(MobEffects.NIGHT_VISION).getDuration();
-		if(time >= t/l) {
-			ci.setReturnValue(Math.min(++i/t, 1.0f)); 
-		} else if(time <= 1.25f) {
-			ci.setReturnValue(i = 0);
-		} else {
-			ci.setReturnValue((1f+(i = Mth.clamp(--i, 0.0f, t))/(1f+t)));
-		}
+		ci.setReturnValue(FadingNightVision.visionProgress);
 	}
-
-	@Override
-	public float overwriteTime(float num) {
-		return t = num;
-	}
-
-	@Override
-	public int overwriteLayer(int num) {
-		return l = num;
-	}
-	
-	
 }
