72a507f388f44f90466fbef03cdf9e36819121ac
Xaikii <xaikii@outlook.de>
2024-07-07 13:48:35 +0200

    1.20.1 port

--- src/main/java/net/perpetualeve/fadingnightvision/mixins/GameRendererMixin.java
+++ src/main/java/net/perpetualeve/fadingnightvision/mixins/GameRendererMixin.java
@@ -2,19 +2,19 @@ package net.perpetualeve.fadingnightvision.mixins;
 
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
-import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
+
+import com.llamalad7.mixinextras.injector.ModifyReturnValue;
 
 import net.minecraft.client.renderer.GameRenderer;
 import net.minecraft.world.entity.LivingEntity;
 import net.perpetualeve.fadingnightvision.FadingNightVision;
 
-@Mixin(GameRenderer.class)
+@Mixin(value = GameRenderer.class, priority = 123153)
 public class GameRendererMixin {
-	
-	@Inject(method = "getNightVisionScale", at = @At("Invoke"), cancellable = true, locals = LocalCapture.CAPTURE_FAILEXCEPTION)
-	private static void getNightVisionScaleRed(LivingEntity p_109109_, float p_109110_, CallbackInfoReturnable<Float> ci) {
-		ci.setReturnValue(FadingNightVision.visionProgress);
+
+	@ModifyReturnValue(method = "getNightVisionScale", at = @At("RETURN"))
+	private static float fnv$tNightVisionScale(float original, LivingEntity p_109109_, float pNanoTime) {
+		FadingNightVision.active = true;
+		return FadingNightVision.visionProgress;
 	}
 }
---
+++ src/main/java/net/perpetualeve/fadingnightvision/mixins/LightTextureMixin.java
@@ -0,0 +1,19 @@
+package net.perpetualeve.fadingnightvision.mixins;
+
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+
+import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
+
+import net.minecraft.client.renderer.LightTexture;
+import net.perpetualeve.fadingnightvision.FadingNightVision;
+
+@Mixin(value = LightTexture.class)
+public class LightTextureMixin {
+
+	@ModifyExpressionValue(method = "updateLightTexture", at = @At(value = "CONSTANT", args = "floatValue=0.0", ordinal = 1))
+	public float fnv$nightVisionEffect(float original) {
+		return Math.max(FadingNightVision.visionProgress, original);
+	}
+	
+}
